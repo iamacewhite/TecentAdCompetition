e19676bcee11ff0036db1ee2f6002caea2e406d4
Xinnan <xduac@connect.ust.hk>
2017-06-01 18:18:39 +0800

    slight change

--- generate_submission.py
+++ generate_submission.py
@@ -3,7 +3,7 @@ import numpy as np
 import pickle
 
 instances = pickle.load(open('instances.p', 'rb'))
-with open('csy/submission') as f:
+with open('submission') as f:
     predictions = [item.strip() for item in f.readlines()]
 result = pd.DataFrame(dict(instanceID = instances, prob = predictions))
 result = result.sort_values(by=['instanceID'])
