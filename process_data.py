9e2766d7974af7c53a94c90bc6b4972f84a4533b
DU-Xinnan <xduac@connect.ust.hk>
2017-06-04 01:10:21 +0800

    add three numerical category to process_data

--- process_data.py
+++ process_data.py
@@ -5,6 +5,7 @@ import os
 import csv
 import hashlib
 import argparse
+
 parser = argparse.ArgumentParser()
 parser.add_argument("mode", help="train or test")
 args = parser.parse_args()
@@ -13,8 +14,11 @@ if args.mode == 'test':
 else:
     path = "joined.csv"
 categorical = ["clickTime", "creativeID", "userID", "positionID", "connectionType", "telecomsOperator", "weekDay", "Hour", "clickTimeCategory", "ageCategory", "adID", "camgaignID", "advertiserID", "Ad_appID", "appPlatform", "gender", "education", "marriageStatus", "haveBaby", "hometown", "residence", "sitesetID", "positionType", "appCategory", "appFirstLevelCategory", "hometownProvince", "residenceProvince", "isHometownAndResidenceProvinceSame"]
-numerical = ["age", "appAdIdCount", "appCampaignIdCount"]
+numerical = ["age", "appAdIdCount", "appCampaignIdCount", "CategoryCount", "appCount", "appUserCount"]
 NR_BINS = 3000000
+
+def decomposeObjStr(string):
+    return map(lambda obj: [obj.split(':')[0].strip(), int(obj.split(':')[1])],string.strip('{}').split(','))
 
 def hashstr(input):
     return str(int(hashlib.md5(input.encode('utf8')).hexdigest(), 16)%(NR_BINS-1)+1)
@@ -30,14 +34,26 @@ def csvToFFM():
     for row in joinedData:
         ffmFile.write(row['label']+" ")
         line = []
+        count = 0
         for index, categoricalLable in enumerate(categorical):
             if (categoricalLable == "clickTime"):
                 row[categoricalLable] = row[categoricalLable][2:4]
-            tempstr = "{0}:{1}:1".format(index, hashstr(categoricalLable+row[categoricalLable]))
+            tempstr = "{0}:{1}:1".format(count, hashstr(categoricalLable+row[categoricalLable]))
             line.append(tempstr)
+            count += 1
         for newIndex, numericalLabel in enumerate(numerical):
-            tempstr = "{0}:{1}:{2}".format(index + newIndex + 1, hashstr(numericalLabel), row[numericalLabel])
-            line.append(tempstr)
+            if (numericalLabel == "CategoryCount"):
+		categoryCount = decomposeObjStr(row[numericalLabel])
+                for arr in categoryCount:
+		    value = arr[1]
+                    key = arr[0]
+                    tempstr = "{0}:{1}:{2}".format(count, hashstr(numericalLabel + key), value)
+                    line.append(tempstr)
+                    count += 1
+            else:
+                tempstr = "{0}:{1}:{2}".format(count, hashstr(numericalLabel), row[numericalLabel])
+                line.append(tempstr)
+                count += 1
         ffmFile.write(" ".join(line) + "\n")
     ffmFile.close()
 
